b40edd6283e39fe2f79b0f6c636a68231ae65836
Ivan Lavrov <ilavrov@yandex-team.ru>
2019-09-29 15:24:30 +0300

    add unit

---
+++ Unit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace flowsharp
+{
+    public class Unit
+    {
+        public static Unit Instance { get; } = new Unit();
+        private Unit()
+        {
+        }
+    }
+}
