fad23abdc5ee3fa98c1b6e68640fbaa7d4c1252d
Ivan Lavrov <ilavrov@yandex-team.ru>
2019-10-01 18:26:11 +0300

    enumerable collector

--- FlowCollector.cs
+++ FlowCollector.cs
@@ -4,13 +4,28 @@ using System.Threading.Tasks;
 
 namespace flowsharp
 {
-    class FlowCollector<T> : IFlowCollector<T>, IAsyncEnumerator<T>
+    class FlowCollector<T> : IFlowCollector<T>
     {
         private readonly Func<T, Task> _handler;
 
         public FlowCollector(Func<T, Task> handler)
         {
             _handler = handler;
+        }
+
+
+        public Task Emit(T item)
+        {
+            return _handler(item);
+        }
+
+    }
+
+    class FlowCollectorEnumerable<T> : FlowCollector<T>, IAsyncEnumerator<T>
+    {
+
+        public FlowCollectorEnumerable(Func<T, Task> handler):base(handler)
+        {
         }
 
         public T Current => throw new NotImplementedException();
@@ -20,15 +35,12 @@ namespace flowsharp
             throw new NotImplementedException();
         }
 
-        public Task Emit(T item)
-        {
-            return _handler(item);
-        }
-
         public ValueTask<bool> MoveNextAsync()
         {
             throw new NotImplementedException();
         }
 
     }
+
+
 }
